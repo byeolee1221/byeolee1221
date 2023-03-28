0658250b7014c1fe427133fc839e361d422a034e
Dominic. Mun <126491953+byeolee1221@users.noreply.github.com>
2023-03-28 18:15:28 +0900

    웹개발 100일 챌린지 - 30일차: 자바스크립트 파일 추가하기, 구문, alert명령어
    30일차에는 자바스크립트의 기초수업으로써 파일 추가하는 방법, 구문, alert() 명령어 및 변수 선언 키워드 let에 대해서 알아보았습니다.
    해당 파일은 오늘 배운 내용을 정리하고, html 및 css로 배운 내용의 주제를 구조화한 파일입니다.
    관련 포스팅: https://itinfogarage.tistory.com/34

---
+++ javascript(3.28).js
@@ -0,0 +1,14 @@
+// let thisUser = "Hi, I'm Byeloee!";
+// let age = 30;
+// alert(thisUser);
+// thisUser = 'Hi, I am really Byeolee!';
+// alert(thisUser);
+// alert(age);
+
+let userName = 'Byeolee';
+let age = 30;
+let hobbies = ['Drum', 'coding', 'game'];
+let job = {title: 'Developer', place: 'Korea', salary: 3000};
+
+alert(hobbies[0]);
+
