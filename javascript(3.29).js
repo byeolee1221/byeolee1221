d46b518acfd2742e99a5a1e4319fb664a4b163fb
Dominic. Mun <126491953+byeolee1221@users.noreply.github.com>
2023-03-29 17:23:57 +0900

    웹개발 100일 챌린지 - 31일차: 객체, 자바스크립트 함수, 함수에서의 값 반환 등
    31일차에는 자바스크립트의 기초를 이어갔습니다.
    객체는 무엇으로 구성되는지, 또 이 객체 안에 있는 값에는 어떻게 접근한는지에 대해 알아보고, 함수에 대해 알아보았습니다. 
    함수는 함수 밖에서 정의된 것을 함수 안에서도 다시 정의할 수 있는데, 이런 변수 섀도잉에 대해서도 공부했고, 함수에서의 값의 반환방식, 함수 매개변수의 사용에 대해서 공부했습니다.
    관련 포스팅: https://itinfogarage.tistory.com/35

---
+++ javascript(3.29).js
@@ -0,0 +1,18 @@
+let adultYears;
+
+// function calculateAdultYears() {
+//     return age - 18;
+// }
+
+function calculateAdultYears(userAge) {
+    return userAge - 18;
+}
+
+// function calculateAdultYears(userAge) {
+//     let result;
+//     result = userAge - 18;
+//     return result;
+// }
+
+adultYears = calculateAdultYears(age);
+alert(adultYears);
