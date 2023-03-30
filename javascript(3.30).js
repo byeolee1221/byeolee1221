b8b8b00805a8b3d7751a82d9109a5a330f15affb
Dominic. Mun <126491953+byeolee1221@users.noreply.github.com>
2023-03-30 19:34:20 +0900

    웹개발 100일 챌린지 - 32일차: 메서드, 자바스크립트에서의 수학연산 등
    32일차에는 메서드라는 것에 대해 알아보고 몇가지 중요 메서드를 알아보고 적용도 해보는 시간을 가졌습니다.
    또, 자바스크립트에서의 수학 연산에 대해 알아보고 원리를 중점적으로 파악하여 적용해보기도 했습니다.
    관련 포스팅: https://itinfogarage.tistory.com/36

---
+++ javascript(3.30).js
@@ -0,0 +1,41 @@
+let greeting = {
+    greet() {
+        console.log('Hello');
+    }
+}
+
+greeting.greet();
+
+console.log((10 + 4) * 3.5 / 10);
+
+let result = (10 + 4) * 3.5 / 10;
+result = 10 * 4;
+result = result + 7;
+console.log(result);
+
+console.log('Byeol' + 'ee');
+
+let stringTest = 'byeol' + '2';
+let stringTest2 = 'byeol' + 2;
+let stringTest3 = 2 + '2';
+let stringTest4 = '2' + '2';
+let stringTest5 = '3' * 2;
+
+console.log(stringTest);
+console.log(stringTest2);
+console.log(stringTest3);
+console.log(stringTest4);
+console.log(stringTest5);
+
+let userText = '안녕하세요. 저는 별이라고 합니다.';
+
+console.log(`문자의 수는 ${userText.length}개입니다.`);
+
+console.log(7 ** 3);
+
+let number = 7;
+
+console.log(number += 5);
+console.log(number -= 5);
+console.log(number *= 5);
+console.log(number /= 5);
