95dd8fe4de6b27d278a083e2bbc08a38411d7180
Dominic. Mun <126491953+byeolee1221@users.noreply.github.com>
2023-04-02 23:18:01 +0900

    개인연습(7-1) - 네모인중개법인 메인페이지 따라 만들어보기
    이번 개인연습에서는 네모인중개법인의 메인페이지를 따라 만들어보았습니다.
    웹사이트에 다양한 기능들이 있는 것 같아 해당 웹사이트를 결정하였으며, 아직 메인섹션의 일부까지만 만들었지만 배운 것이 참 많은 개인연습이었습니다.
    또한, 이 개인연습에서는 자바스크립트를 적용하여 HTML과 연결해 보았습니다.
    관련 포스팅: https://itinfogarage.tistory.com/39

---
+++ nemoin.js
@@ -0,0 +1,14 @@
+// header섹션 좌측 링크
+
+document.getElementById('nemo_service').href = 'https://www.nemoin.co.kr/service/nemo';
+
+document.getElementById('nemo_exclusive').href = 'https://www.nemoin.co.kr/service/exclusive';
+
+document.getElementById('nemo_recruit').href = 'https://www.nemoin.co.kr/recruit';
+
+document.getElementById('nav_left_third').href = 'https://abaft-periodical-fb0.notion.site/welcome-to-NEMOIN-924707464e044551a1c7ac4c90973788';
+
+// header섹션 우측 링크
+
+document.getElementById('nav_right_first').href = 'https://www.nemoapp.kr/Search?ArticleType=1&PageIndex=0&SWLng=126.98073678092739&SWLat=37.47175971759283&NELng=127.15451594898843&NELat=37.53919955456909&Zoom=14&mode=1&category=1&list=true&articleId=&dataType=';
+
