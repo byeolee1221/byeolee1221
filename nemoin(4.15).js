770ca47031a0b3ffa4d568a693e896468ea0c821
Dominic. Mun <126491953+byeolee1221@users.noreply.github.com>
2023-04-15 21:07:06 +0900

    개인연습(7-3) - 네모인 메인페이지 완성하기
    오늘은 저번에 7번째로 연습한 네모인 메인페이지 연습을 끝내보았습니다.
    이번 연습으로 새로운 CSS 지식들과 HTML 지식들을 터득하게 되어 여러모로 좋았던 연습이었습니다.
    추후 이 메인페이지의 '매물 문의하기' 버튼을 누르면 오버레이창이 뜨는 것을 자바스크립트로 구현해서 올려볼 예정입니다.
    관련 포스팅: https://itinfogarage.tistory.com/51

---
+++ nemoin(4.15).js
@@ -0,0 +1,28 @@
+// header섹션 좌측 링크
+
+document.getElementById('nemo_service').href = 'https://www.nemoin.co.kr/service/nemo';
+
+document.getElementById('nemo_exclusive').href = 'https://www.nemoin.co.kr/service/exclusive';
+
+document.getElementById('nemo_recruit').href = 'https://www.nemoin.co.kr/recruit';
+
+document.getElementById('nav_left_third').href = 'https://abaft-periodical-fb0.notion.site/welcome-to-NEMOIN-924707464e044551a1c7ac4c90973788';
+
+// header섹션 우측 링크
+
+document.getElementById('nav_right_first').href = 'https://www.nemoapp.kr/Search?ArticleType=1&PageIndex=0&SWLng=126.98073678092739&SWLat=37.47175971759283&NELng=127.15451594898843&NELat=37.53919955456909&Zoom=14&mode=1&category=1&list=true&articleId=&dataType=';
+
+// main섹션 '서비스 바로가기' 링크
+
+document.getElementById('first_bottom').href = 'https://nemoapp.kr/';
+
+// main섹션 '네모 자회사' 링크
+
+document.getElementById('nc_nemoin').href = '/#'
+document.getElementById('nc_nemo').href = 'https://www.nemoapp.kr/';
+document.getElementById('nc_zigbang').href = 'https://www.zigbang.com/';
+document.getElementById('nc_daum').href = 'https://realty.daum.net/';
+
+// footer섹션 '개인정보 처리방침' 링크
+
+document.getElementById('footer_privacy').href = 'https://www.nemoin.co.kr/policy/privacyPolicy.html?v=2022100501';
