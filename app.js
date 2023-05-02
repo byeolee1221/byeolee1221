426b1b9385fa3fe0ccdcef3a62f17b25d1b8c494
Dominic. Mun <126491953+byeolee1221@users.noreply.github.com>
2023-05-02 21:50:18 +0900

    51일차 - EJS의 include 기능, 동적 경로 만들기, 고유 id로 데이터 관리
    51일차에는 EJS 엔진을 이용하여 개발 시에 느끼는 번거로움을 덜어주는 include 기능에 대해 알아보고, 사용자를 위한 동적 경로를 고유 id를 활용하여 만들어보는 시간을 가졌습니다.
    이 과정에서 코드를 관리하는 것을 습관화해야겠다는 생각이 들었습니다. 코드가 너무 많아지면 여러 기능을 활용하여 분산하는 것이 개발자 본인뿐만 아니라 다른 사람에게도 도움이 되기 때문입니다.
    관련 포스팅: https://itinfogarage.tistory.com/67

--- app.js
+++ app.js
@@ -1,18 +1,63 @@
-let paragraghElement = document.querySelector('.click');
+const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const uuid = require('uuid');
 
-function changeText() {
-    paragraghElement.textContent = 'Clicked!';
-    console.log('Paragragh Clicked!');
-}
+const app = express();
 
-paragraghElement.addEventListener('click', changeText);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-let userMessage = document.getElementById('user_message');
+app.use(express.static('public'));
+app.use(express.urlencoded({extended: false}));
 
-function message(events) {
-    let enteredText = userMessage.value;
-    console.log(enteredText);
-    console.log(events);
-}
+app.get('/', function(req, res) {
+    res.render('Eatwell');
+})
 
-userMessage.addEventListener('input', message);
+app.get('/Restaurants', function(req, res) {
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+    res.render('Restaurants', {numberOfRestaurants: storedRestaurants.length, restaurants: storedRestaurants});
+})
+
+app.get('/Restaurants/:id', function(req, res) {
+    const restaurantId = req.params.id;
+    res.render('Restaurants-detail', {rid: restaurantId});
+})
+
+// app.get('/Restaurants/:website', function(req, res) {
+//     const restaurantWebsite = req.params.website;
+//     res.render('Restaurants-detail', {site: restaurantWebsite});
+// })
+
+app.get('/recommend', function(req, res) {
+    res.render('recommend');
+})
+
+app.post('/recommend', function(req, res) {
+    const restaurant = req.body;
+    restaurant.id = uuid.v4();
+    const filePath = path.join(__dirname, 'data', 'restaurants.json');
+
+    const fileData = fs.readFileSync(filePath);
+    const storedRestaurants = JSON.parse(fileData);
+
+    storedRestaurants.push(restaurant);
+
+    fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
+
+    res.redirect('/confirm');
+})
+
+app.get('/about', function(req, res) {
+    res.render('about');
+})
+
+app.get('/confirm', function(req, res) {
+    res.render('confirm');
+})
+
+app.listen(3000);
