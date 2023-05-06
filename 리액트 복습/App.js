fed917cdea41fc15447d6bbaac477ef5f140e375
Dominic. Mun <126491953+byeolee1221@users.noreply.github.com>
2023-05-06 21:42:41 +0900

    리액트 8일차 - 리액트 처음부터 다시 복습하기
    8일차에는 새로운 진도를 나가지 않고, 리액트 강의를 처음부터 다시 수강하며 이해되지 않는 부분을 잡는 과정을 시작했습니다.
    해당 파일들은 수업을 진행하며 강사와 동시에 웹페이지를 다듬지 않고, 강사의 구현이 끝나면 제가 처음부터 직접 만들어보는 방식으로 진행한 파일입니다. 
    관련 포스팅: https://itinfogarage.tistory.com/70

---
+++ 리액트 복습/App.js
@@ -0,0 +1,23 @@
+import ExpenseHeader from "./components/ExpenseHeader";
+import ExpenseMainTitle from "./components/ExpenseMainTitle";
+import ExpenseExampleData from "./components/ExpensesExampleData";
+import "./App.css";
+
+const App = () => {
+  const expensesExample = [
+    {itemName: '커피', price: 9500, date: new Date('2020-01-26')},
+    {itemName: '스피커', price: 106000, date: new Date('2021-04-16')},
+    {itemName: '로션', price: 12000, date: new Date('2022-07-05')},
+    {itemName: '컴퓨터부품', price: 3300000, date: new Date('2023-02-20')}
+  ];
+
+  return (
+    <div className="App">
+      <ExpenseHeader />
+      <ExpenseMainTitle />
+      <ExpenseExampleData item={expensesExample} />
+    </div>
+  );
+}
+
+export default App;
---
+++ 리액트 복습/components/ExpenseHeader.js
@@ -0,0 +1,14 @@
+import "./ExpenseHeader.css";
+
+const ExpenseHeader = () => {
+    return (
+        <header className="header_section">
+            <div className="header_container">
+                <h1>Welcome to React Exercise Webpage</h1>
+                <p>This webpage contains several components about expense.</p>
+            </div>
+        </header>
+    );
+}
+
+export default ExpenseHeader;
