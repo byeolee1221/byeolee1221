b8b3a9ab72a5c99cd00c0e0c398c27d03478fb08
Dominic. Mun <126491953+byeolee1221@users.noreply.github.com>
2023-05-10 20:03:24 +0900

    리액트 11일차 - key 속성, 선택한 연도의 리스트 표출, 조건부 내용 출력
    <웹페이지 변경사항>
    🔎 key속성을 사용하여 배열 내 데이터에 고유 id를 부여해주어 웹페이지 안정성을 높였습니다.
    🔎 사용자가 선택한 연도에 해당하는 지출리스트만 표출하도록 수정해주었습니다.
    🔎 사용자가 원할 때 버튼을 눌러 지출내역을 추가할 수 있도록 변경했습니다. 또, 사용자가 새 데이터를 추가하면 지출내역 입력양식을 닫을 수 있도록 해주었습니다.
    🔎 관련 포스팅: https://itinfogarage.tistory.com/73

--- 리액트 복습/App.js
+++ 리액트 복습/App.js
@@ -7,7 +7,7 @@ import ExpenseListFilter from "./components/ExpenseListFilter";
 import "./App.css";
 
 const expensesDummy = [
-  {id: 'ex1', itemName: '커피', price: 9500, date: new Date('2020-01-26')},
+  {id: 'ex1', itemName: '커피', price: 9500, date: new Date('2021-01-26')},
   {id: 'ex2', itemName: '스피커', price: 106000, date: new Date('2021-04-16')},
   {id: 'ex3', itemName: '로션', price: 12000, date: new Date('2022-07-05')},
   {id: 'ex4', itemName: '컴퓨터부품', price: 3300000, date: new Date('2023-02-20')}
@@ -28,13 +28,17 @@ const App = () => {
     setSelectedYear(userYear);
   }
 
+  const filterExpenseList = expense.filter((ExpenseFilter) => {
+    return ExpenseFilter.date.getFullYear().toString() === selectedYear;
+})
+
   return (
     <div className="App">
       <ExpenseHeader />
       <ExpenseUser onAddList={addExpenseListHandler} />
       <ExpenseMainTitle />
       <ExpenseListFilter selectYear={selectedYear} onFilterChange={filterHandler} />
-      <ExpenseExampleData item={expense} />
+      <ExpenseExampleData item={expense} onFilterExpenseList={filterExpenseList} />
     </div>
   );
 }
--- 리액트 복습/ExpensesExampleData.js
+++ 리액트 복습/ExpensesExampleData.js
@@ -2,13 +2,24 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpenseExampleData.css";
 
 const ExpenseExampleData = (props) => {
-    return (
-        <div className="main_expanselist_data">
-            {props.item.map((expensesExample) => 
-                <ExpenseItem itemName={expensesExample.itemName} price={expensesExample.price} date={expensesExample.date} />
-            )}
-        </div>
-    );
-}
+  let expenseListMsg = <p>해당 연도의 지출내역이 없습니다.</p>;
+
+  if (props.onFilterExpenseList.length > 0) {
+        expenseListMsg = props.onFilterExpenseList.map((expensesExample) => (
+            <ExpenseItem
+                key={expensesExample.id}
+                itemName={expensesExample.itemName}
+                price={expensesExample.price}
+                date={expensesExample.date}
+            />
+        )); 
+  }
+
+  return (
+    <div className="main_expanselist_data">
+      {expenseListMsg}
+    </div>
+  );
+};
 
 export default ExpenseExampleData;
