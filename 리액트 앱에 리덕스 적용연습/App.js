e3f2b56ba946083842cef0e91ff4f5f36bff7aa8
Dominic. Mun <126491953+byeolee1221@users.noreply.github.com>
2023-06-21 20:10:36 +0900

    개인연습(13) - 리액트앱에 리덕스 적용연습
    🔎 오늘은 어제 배운 것을 바탕으로, 어제와 비슷한 리액트앱을 만들어 리덕스를 적용해보았습니다.
    🔎 이 연습을 함으로 인해 리액트앱에 리덕스를 적용하는 것에 대해서 조금 더 알게되었습니다.
    🔎 관련 포스팅: https://itinfogarage.tistory.com/105

---
+++ 리액트 앱에 리덕스 적용연습/App.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+
+import Counter from './components/Counter';
+import './App.css';
+
+function App() {
+  return (
+    <Fragment>
+      <Counter />
+    </Fragment>
+  );
+}
+
+export default App;
